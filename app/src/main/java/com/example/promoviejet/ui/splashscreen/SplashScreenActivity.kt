42b95afea4838e6a626b8c24916fa84ef31681f7
anasmisbah <anas.misbahuddin4869@gmail.com>
2019-12-19 19:28:13 +0800

    add anim splashscreen

--- app/src/main/java/com/example/promoviejet/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/promoviejet/ui/splashscreen/SplashScreenActivity.kt
@@ -3,8 +3,11 @@ package com.example.promoviejet.ui.splashscreen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.promoviejet.R
 import com.example.promoviejet.ui.home.HomeActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
@@ -18,6 +21,8 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val anim = AnimationUtils.loadAnimation(this@SplashScreenActivity,R.anim.fromtop)
+        logo_splashscreen.animation = anim
         CoroutineScope(IO).launch {
             addDelaySplashScreen()
             goToHome()
@@ -32,6 +37,6 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private suspend fun addDelaySplashScreen(){
-        delay(3000)
+        delay(2000)
     }
 }
